1ec479caf194a215b02797072d17044bf12a15ae
G-Sree-krupa <137261202+G-Sree-krupa@users.noreply.github.com>
2025-03-23 17:05:47 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div>
+      <b><h1 align="center">Welcome to our zomato app</h1></b>
+      <div class="image-container"></div>
+      <img src="./zomato pic.jpg" alt="" style={{ width: "1020px", height:" 400px"}}/>
+      <b><div class="text-overlay">Order food from favourite restaurants near you</div></b>
+      </div>
+  )
+}
+export default Home
